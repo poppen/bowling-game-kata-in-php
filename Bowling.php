850cc9a2681e676a30357b213feed9b3d4f637b9
poppen <poppen.jp@gmail.com>
2013-03-27 22:06:26 +0900

    Refactor score()

--- Bowling.php
+++ Bowling.php
@@ -18,10 +18,10 @@ class Bowling
     {
         $score = 0;
         for ($index=0; $index < count($this->throws); $index++) {
-            if ($this->throws[$index] == 10) {
-                $score += (10 + $this->throws[$index+1] + $this->throws[$index+2]);
-            } else if ($this->throws[$index] + $this->throws[$index+1] == 10) {
-                $score += (10 + $this->throws[$index+2]);
+            if ($this->is_strike($index)) {
+                $score += $this->strike_score($index);
+            } else if ($this->is_spare($index)) {
+                $score += $this->spare_score($index);
                 $index += 1;
             } else {
                 $score += ($this->throws[$index] + $this->throws[$index+1]);
@@ -29,6 +29,26 @@ class Bowling
             }
         }
         return $score;
+    }
+
+    private function is_strike($index)
+    {
+        return $this->throws[$index] == 10;
+    }
+
+    private function is_spare($index)
+    {
+        return $this->throws[$index] + $this->throws[$index+1] == 10;
+    }
+
+    private function strike_score($index)
+    {
+        return 10 + $this->throws[$index+1] + $this->throws[$index+2];
+    }
+
+    private function spare_score($index)
+    {
+        return 10 + $this->throws[$index+2];
     }
 }
 ?>
